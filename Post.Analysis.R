526c61ba57df8577131bfaf8bca51753d2b3602c
josiah-leinbach <98821696+josiah-leinbach@users.noreply.github.com>
2024-06-24 18:29:49 -0400

    Add files via upload

---
+++ Post.Analysis.R
@@ -0,0 +1,22 @@
+# Import RDS files
+rds_1.1 <- readRDS("EMR-R1.1-R2.1-9pos.rds")
+rds_1.2 <- readRDS("EMR-R1.1-R2.2-9pos.rds")
+rds_2.1 <- readRDS("EMR-R1.2-R2.1-9pos.rds")
+rds_2.2 <- readRDS("EMR-R1.2-R2.2-9pos.rds")
+rds_3.1 <- readRDS("EMR-R1.3-R2.1-9pos.rds")
+rds_3.2 <- readRDS("EMR-R1.3-R2.2-9pos.rds")
+rds_3.3 <- readRDS("EMR-R1.3-R2.3-9pos.rds")
+
+# Build matrix
+BIC_mat <- matrix(data = NA, nrow = 3, ncol = 3)
+BIC_mat[1,1] <- rds_1.1$BIC
+BIC_mat[1,2] <- rds_1.2$BIC
+BIC_mat[2,1] <- rds_2.1$BIC
+BIC_mat[2,2] <- rds_2.2$BIC
+BIC_mat[3,1] <- rds_3.1$BIC
+BIC_mat[3,2] <- rds_3.2$BIC
+BIC_mat[3,3] <- rds_3.3$BIC
+
+rownames(BIC_mat) <- c("R1 = 1", "R1 = 2", "R1 = 3")
+colnames(BIC_mat) <- c("R2 = 1", "R2 = 2", "R2 = 3")
+
