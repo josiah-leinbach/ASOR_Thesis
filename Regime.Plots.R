2bef4d032ed568261dbc504e1561ec9f2d697ed7
josiah-leinbach <98821696+josiah-leinbach@users.noreply.github.com>
2024-08-19 17:27:24 -0400

    Update Regime.Plots.R

--- Regime.Plots.R
+++ Regime.Plots.R
@@ -1,10 +1,10 @@
 library(ggplot2)
 
-rds_2.1 <- readRDS("EMR-R1.2-R2.1-9pos.rds")
+rds_2.1 <- readRDS("EMR-R1.2-R2.1_731.rds") # 1611 second-lowest
 id <- rds_2.1$id
 
-rds.HJ1_2.2 <- readRDS("EMR.HJ-R1.2-R2.2-9pos.rds")
-id.HJ1 <- rds.HJ1_2.2$id
+rds.HJ_2.1 <- readRDS("EMR.HJ123-R1.2-R2.1-1611.rds")
+id.HJ <- rds.HJ1_2.1$id
 
 ### Baseline for 13 book Pauline Corpus ###
 N <- numeric(13)
@@ -28,28 +28,36 @@ ggplot(main_df, aes(x = Sentence, y = Book, fill = factor(ID))) +
   geom_tile(width = 1, height = 1) +
   scale_fill_manual(values = c("1" = "lightblue", "2" = "blue", "3" = "orange"))
 
-### Hebrews + 1 John ###
-N.2 <- numeric(15)
-for (i in 1:15) {
-  N.2[i] <- sum(!is.na(id.HJ1[i,]))
+### Hebrews + Johannine ###
+N.2 <- numeric(17)
+for (i in 1:17) {
+  N.2[i] <- sum(!is.na(id.HJ[i,]))
 }
 
 books_HJ <- c("Rom", "Cor.1", "Cor.2", "Gal", "Php", "Thes.1", "Phm", "Eph", 
-              "Col", "Thes.2", "Tim.1", "Tim.2", "Tit", "Heb", "John.1")
+              "Col", "Thes.2", "Tim.1", "Tim.2", "Tit", "Heb", "John.1", "John.2", "John.3")
 main_df.2 <- data.frame()
 
-for (i in 1:15) {
+for (i in 1:17) {
   book_vec <- rep(books_HJ[i], N.2[i])
   sent_vec <- c(1:N.2[i])
-  ID_vec <- id.HJ1[i,1:N.2[i]]
+  ID_vec <- id.HJ[i,1:N.2[i]]
   book_df <- data.frame(Book = book_vec, Sentence = sent_vec, ID = ID_vec)
   main_df.2 <- rbind(main_df.2, book_df)
 }
 
 # Sequence plot 
 ggplot(main_df.2, aes(x = Sentence, y = Book, fill = factor(ID))) +
-  ggtitle("Regime Distributions for 13 Book Claimed Pauline Corpus + Hebrews and 1 John", subtitle = "R1 = 2, R2 = 2") +
+  ggtitle("Regime Distributions for 13 Book Claimed Pauline Corpus + Hebrews and Johnannine Letters", subtitle = "R1 = 2, R2 = 1") +
   geom_tile(width = 1, height = 1) +
   scale_fill_manual(values = c("1" = "lightblue", "2" = "blue", "3" = "orange", "4" ="gold4"))
 
 
+### Cumulative average sequence plots ###
+library(tidyr)
+Tim.1_long <- book_prop[[11]][,-1] %>%
+  gather(key = "Regime", value = "Prop", -Sentence)
+
+ggplot(test_plot.long, aes(x = Sentence, y = Prop, fill = Regime)) +
+  geom_col(position = "fill") + 
+  scale_y_continuous(labels = scales::percent)
