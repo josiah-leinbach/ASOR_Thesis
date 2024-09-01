44533b4333a79ab23b52fc0881c13dbdd62c134a
josiah-leinbach <98821696+josiah-leinbach@users.noreply.github.com>
2024-08-31 22:05:52 -0400

    Update Regime.Plots.R

--- Regime.Plots.R
+++ Regime.Plots.R
@@ -1,58 +1,87 @@
 library(ggplot2)
 
-setwd("C:/Users/jleinba/Documents/ASOR/ASOR_Thesis/Results/R1.2_R2.1")
-rds_2.1 <- readRDS("EMR-R1.2-R2.1_731.rds") # 1611 second-lowest
-id <- rds_2.1$id
-
-setwd("C:/Users/jleinba/Documents/ASOR/ASOR_Thesis/Results/HJ123_R1.2_R2.1")
-rds.HJ_2.1 <- readRDS("EMR.HJ123-R1.2-R2.1-1348.rds") # 1707 second-lowest
-id.HJ <- rds.HJ_2.1$id
-
-### Baseline for 13 book Pauline Corpus ###
-N <- numeric(13)
-for (i in 1:13) {
-  N[i] <- sum(!is.na(id[i,]))
-}
+R1.2_R2.1 <- list.files("/Users/josiahleinbach/Downloads/R1.2_R2.1","EMR")
+N <- c(331, 377, 174, 110, 55, 50, 10, 55, 39, 25, 61, 46, 27)
 books <- c("Rom", "Cor.1", "Cor.2", "Gal", "Php", "Thes.1", "Phm", "Eph", "Col", "Thes.2", "Tim.1", "Tim.2", "Tit")
-main_df <- data.frame()
 
-for (i in 1:13) {
-  book_vec <- rep(books[i], N[i])
-  sent_vec <- c(1:N[i])
-  ID_vec <- id[i,1:N[i]]
-  book_df <- data.frame(Book = book_vec, Sentence = sent_vec, ID = ID_vec)
-  main_df <- rbind(main_df, book_df)
+prop_amb <- numeric(25)
+BIC_r1.2_r2.1 <- round(BIC_r1.2_r2.1, 2)
+
+file.num <- c(1066,1170,1215,1348,1415,1534,1556,1588,1605,1611,1660,1662,1688,
+              1697,1704,1707,1741,1801,1805,1815,1945,2022,43,597,731)
+
+amb_func <- function(x) {
+  z <- numeric()
+  y <- order(x, decreasing = T)
+  if (sum(x > 0.7) == 1) {
+    z <- as.character(y[1])
+  } else if (y[1] == 1 & y[2] == 2) {
+    z <- as.character("1_2") # 1 and 2
+  } else if (y[1] == 2 & y[2] == 1) {
+    z <- as.character("1_2")
+  } else if (y[1] == 2 & y[2] == 3) {
+    z <- as.character("2_3") # 2 and 3
+  } else if (y[1] == 3 & y[2] == 2) {
+    z <- as.character("2_3")
+  } else if (y[1] == 1 & y[2] == 3) {
+    z <- as.character("1_3") # 1 and 3
+  } else {
+    z <- as.character("1_3")
+  }
+  return(z)
 }
 
-# Sequence plot 
-ggplot(main_df, aes(x = Sentence, y = Book, fill = factor(ID))) +
-  ggtitle("Regime Distributions for 13 Book Claimed Pauline Corpus", subtitle = "R1 = 2, R2 = 1. BIC = 118,855.6") +
+for (w in 1:25) {
+  main_df <- data.frame()
+  rds <- readRDS(R1.2_R2.1[w])
+  rds_gamma <- rds$gamma
+  for (i in 1:13) {
+    gamma_book <- round(rds_gamma[i,1:N[i],],2)
+    gamma_prob <- gamma_book > 0.7
+    gamma_order <- t(apply(gamma_book, MARGIN = 1, FUN = order))
+    results_vec <- factor(apply(gamma_book, MARGIN = 1, FUN = amb_func))
+    book_vec <- rep(books[i], N[i])
+    sent_vec <- c(1:N[i])
+    main_df.sub <- data.frame(Book = book_vec, Sentence = sent_vec, ID = results_vec)
+    main_df <- rbind(main_df, main_df.sub)
+  }
+  assign(paste0("Seed_",file.num[w]), main_df)
+  prop_amb[w] <- length(which(main_df$ID == "1_2" |
+                                main_df$ID == "2_3" |
+                                main_df$ID == "1_3")) / sum(N)
+}
+
+bic_df <- data.frame(BIC = BIC_r1.2_r2.1, Prop_Amb = prop_amb)
+plot(bic_df$BIC, bic_df$Prop_Amb, xlab =  "BIC", ylab = "Prop_Amb", 
+     main = "Proportion of Ambiguous Sentences by BIC")
+
+summary(lm(Prop_Amb ~ BIC, data = bic_df[-c(11,12,20)])) # Removing high leverage points
+
+# Sequence plots
+R1.2_R2.1[order(BIC_r1.2_r2.1)] # Find order of BIC
+ggplot(Seed_731, aes(x = Sentence, y = Book, fill = factor(ID))) +
+  ggtitle("Regime Distributions for 13 Book Claimed Pauline Corpus", 
+          subtitle = paste0("Seed_", file.num[25], "(", "BIC = ", BIC_r1.2_r2.1[25], ")")) +
   geom_tile(width = 1, height = 1) +
-  scale_fill_manual(values = c("1" = "lightblue", "2" = "blue", "3" = "orange"))
+  scale_fill_manual(values = c("1" = "lightblue", "2" = "blue", 
+                               "3" = "orange", "1_2" = "forestgreen",
+                               "1_3" = "yellow", "2_3" = "violet"))
 
-### Hebrews + Johannine ###
-N.2 <- numeric(17)
-for (i in 1:17) {
-  N.2[i] <- sum(!is.na(id.HJ[i,]))
-}
-
-books_HJ <- c("Rom", "Cor.1", "Cor.2", "Gal", "Php", "Thes.1", "Phm", "Eph", 
-              "Col", "Thes.2", "Tim.1", "Tim.2", "Tit", "Heb", "John.1", "John.2", "John.3")
-main_df.2 <- data.frame()
-
-for (i in 1:17) {
-  book_vec <- rep(books_HJ[i], N.2[i])
-  sent_vec <- c(1:N.2[i])
-  ID_vec <- id.HJ[i,1:N.2[i]]
-  book_df <- data.frame(Book = book_vec, Sentence = sent_vec, ID = ID_vec)
-  main_df.2 <- rbind(main_df.2, book_df)
-}
-
-# Sequence plot 
-ggplot(main_df.2, aes(x = Sentence, y = Book, fill = factor(ID))) +
-  ggtitle("Regime Distributions for 13 Book Claimed Pauline Corpus + Hebrews and Johnannine Letters", subtitle = "R1 = 2, R2 = 1") +
+ggplot(Seed_1688, aes(x = Sentence, y = Book, fill = factor(ID))) +
+  ggtitle("Regime Distributions for 13 Book Claimed Pauline Corpus", 
+          subtitle = paste0("Seed_", file.num[13], "(", "BIC = ", BIC_r1.2_r2.1[13], ")")) +
   geom_tile(width = 1, height = 1) +
-  scale_fill_manual(values = c("1" = "lightblue", "2" = "blue", "3" = "orange", "4" ="gold4"))
+  scale_fill_manual(values = c("1" = "lightblue", "2" = "blue", 
+                               "3" = "orange", "1_2" = "forestgreen",
+                               "1_3" = "yellow", "2_3" = "violet"))
+
+ggplot(Seed_1662, aes(x = Sentence, y = Book, fill = factor(ID))) +
+  ggtitle("Regime Distributions for 13 Book Claimed Pauline Corpus", 
+          subtitle = paste0("Seed_", file.num[12], "(", "BIC = ", BIC_r1.2_r2.1[12], ")")) +
+  geom_tile(width = 1, height = 1) +
+  scale_fill_manual(values = c("1" = "lightblue", "2" = "blue", 
+                               "3" = "orange", "1_2" = "forestgreen",
+                               "1_3" = "yellow", "2_3" = "violet"))
 
 
 ### Cumulative classification sequence plots ###
