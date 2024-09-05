582bbc0fdb5df62a4ce06e5c9051f88ffb2ea312
josiah-leinbach <98821696+josiah-leinbach@users.noreply.github.com>
2024-09-05 19:35:43 -0400

    Update Regime.Plots.R

--- Regime.Plots.R
+++ Regime.Plots.R
@@ -1,11 +1,15 @@
 library(ggplot2)
 
-R1.2_R2.1 <- list.files("C:/Users/jleinba/Documents/ASOR_Thesis/Results/6-POS/R1.2-R2.1/EPS_10e-6","EMR")
+R1.2_R2.2 <- list.files("C:/Users/jleinba/Documents/ASOR/ASOR_Thesis/Results/6-POS/R1.2-R2.2/EPS_10e-6","EMR")
 N <- c(331, 377, 174, 110, 55, 50, 10, 55, 39, 25, 61, 46, 27)
 books <- c("Rom", "Cor.1", "Cor.2", "Gal", "Php", "Thes.1", "Phm", "Eph", "Col", "Thes.2", "Tim.1", "Tim.2", "Tit")
 
+BIC_r1.2_r2.2 <- numeric()
+for (i in 1:25) {
+  rds <- readRDS(R1.2_R2.2[i])
+  BIC_r1.2_r2.2[i] <- round(rds$BIC,2)
+}
 prop_amb <- numeric(25)
-BIC_r1.2_r2.1 <- round(bic_vec, 2)
 
 file.num <- c(1066,1170,1215,1348,1415,1534,1556,1588,1605,1611,1660,1662,1688,
               1697,1704,1707,1741,1801,1805,1815,1945,2022,43,597,731)
@@ -50,7 +54,7 @@ for (i in 1:13) {
 
 for (w in 1:25) {
   main_df <- data.frame()
-  rds <- readRDS("EMR-R1.2-R2.2_NEW_731.rds")
+  rds <- readRDS(file.names[w])
   rds_gamma <- rds$gamma
   for (i in 1:13) {
     gamma_book <- round(rds_gamma[i,1:N[i],],2)
@@ -62,10 +66,10 @@ for (w in 1:25) {
     main_df.sub <- data.frame(Book = book_vec, Sentence = sent_vec, ID = results_vec)
     main_df <- rbind(main_df, main_df.sub)
   }
-  assign("Seed_731", main_df)
-  prop_amb[w] <- length(which(main_df$ID == "1_2" |
-                                main_df$ID == "2_3" |
-                                main_df$ID == "1_3")) / sum(N)
+  main_df$ID <- factor(main_df$ID, levels = c("1", "1_2", "2",
+                                              "1_3", "1_4", "2_3", "2_4", 
+                                              "3", "3_4", "4"))
+  assign(paste0("Seed_", file.num[w]), main_df)
 }
 
 for (i in 1:13) {
@@ -85,57 +89,139 @@ for (i in 1:13) {
   assign(book_long[i], book_df)
 }
 
-bic_df <- data.frame(BIC = BIC_r1.2_r2.1, Prop_Amb = prop_amb)
-plot(bic_df$BIC, bic_df$Prop_Amb, xlab =  "BIC", ylab = "Prop_Amb", 
-     main = "Proportion of Ambiguous Sentences by BIC")
-
-summary(lm(Prop_Amb ~ BIC, data = bic_df[-c(11,12,20)])) # Removing high leverage points
-
 # Sequence plots
-R1.2_R2.1[order(BIC_r1.2_r2.1)] # Find order of BIC
-ggplot(Seed_43, aes(x = Sentence, y = Book, fill = factor(ID))) +
+R1.2_R2.2[order(BIC_r1.2_r2.2)] # Find order of BIC
+ggplot(Seed_1660, aes(x = Sentence, y = Book, fill = factor(ID))) +
   ggtitle("Regime Distributions for 13 Book Claimed Pauline Corpus") + 
-  geom_tile(width = 1, height = 1)
+  geom_tile(width = 1, height = 1) + 
+  scale_fill_manual(values = c("1" = "blue", "2" = "lightblue", "1_2" = "dodgerblue",
+                               "1_3" = "violet", "1_4" = "maroon2", "2_3" = "pink",
+                               "2_4" = "purple", "3" = "orange", "3_4" = "forestgreen", "4" = "red"))
+
+ggplot(Seed_1741, aes(x = Sentence, y = Book, fill = factor(ID))) +
+  ggtitle("Regime Distributions for 13 Book Claimed Pauline Corpus") +
+  geom_tile(width = 1, height = 1) +
+  scale_fill_manual(values = c("1" = "blue", "2" = "lightblue", "1_2" = "dodgerblue",
+                               "1_3" = "violet", "1_4" = "maroon2", "2_3" = "pink",
+                               "2_4" = "purple", "3" = "orange", "3_4" = "forestgreen", "4" = "red"))
 
 ggplot(Seed_597, aes(x = Sentence, y = Book, fill = factor(ID))) +
   ggtitle("Regime Distributions for 13 Book Claimed Pauline Corpus") +
-  geom_tile(width = 1, height = 1)
-
-ggplot(Seed_731, aes(x = Sentence, y = Book, fill = factor(ID))) +
-  ggtitle("Regime Distributions for 13 Book Claimed Pauline Corpus") +
-  geom_tile(width = 1, height = 1)
+  geom_tile(width = 1, height = 1) + 
+  geom_tile(width = 1, height = 1) +
+  scale_fill_manual(values = c("1" = "blue", "2" = "lightblue", "1_2" = "dodgerblue",
+                               "1_3" = "violet", "1_4" = "maroon2", "2_3" = "pink",
+                               "2_4" = "purple", "3" = "orange", "3_4" = "forestgreen", "4" = "red"))
 
 
 ### Cumulative classification sequence plots ###
 library(tidyr)
+library(dplyr)
 # 1 Timothy
 Tim.1_long <- Tim.1.long %>%
   count(Sentence, Regime) %>%
   complete(Sentence, Regime, fill = list(n = 0))
 
-Tim.1_long$Regime <- factor(Tim.1_long$Regime, levels = c("1_3", "2_3", "3", "1_2", "2", "1"))
+Tim.1_long$Regime <- factor(Tim.1_long$Regime, levels = c("1", "1_2", "2",
+                                                          "1_3", "1_4", "2_3", "2_4", 
+                                                          "3", "3_4", "4"))
 names(Tim.1_long)[3] <- "Total"
 
 ggplot(Tim.1_long, aes(x = Sentence, y = Total, fill = Regime)) +
   geom_col(position = "fill") + 
   scale_y_continuous(labels = scales::percent) +
-  scale_fill_manual(values = c("1" = "lightblue", "2" = "blue", 
-                               "3" = "orange", "1_2" = "forestgreen",
-                               "1_3" = "yellow", "2_3" = "violet")) +
+  scale_fill_manual(values = c("1" = "blue", "2" = "lightblue", "1_2" = "dodgerblue",
+                               "1_3" = "violet", "1_4" = "maroon2", "2_3" = "pink",
+                               "2_4" = "purple", "3" = "orange", "3_4" = "forestgreen", "4" = "red")) +
   labs(title = "1 Timothy (13 Book Pauline Corpus)")
+
+# 2 Timothy
+Tim.2_long <- Tim.2.long %>%
+  count(Sentence, Regime) %>%
+  complete(Sentence, Regime, fill = list(n = 0))
+
+Tim.2_long$Regime <- factor(Tim.2_long$Regime, levels = c("1", "1_2", "2",
+                                                          "1_3", "1_4", "2_3", "2_4", 
+                                                          "3", "3_4", "4"))
+names(Tim.2_long)[3] <- "Total"
+
+ggplot(Tim.2_long, aes(x = Sentence, y = Total, fill = Regime)) +
+  geom_col(position = "fill") + 
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_manual(values = c("1" = "blue", "2" = "lightblue", "1_2" = "dodgerblue",
+                               "1_3" = "violet", "1_4" = "maroon2", "2_3" = "pink",
+                               "2_4" = "purple", "3" = "orange", "3_4" = "forestgreen", "4" = "red")) +
+  labs(title = "2 Timothy (13 Book Pauline Corpus)")
+
+# Titus
+Tit_long <- Tit.long %>%
+  count(Sentence, Regime) %>%
+  complete(Sentence, Regime, fill = list(n = 0))
+
+Tit_long$Regime <- factor(Tit_long$Regime, levels = c("1", "1_2", "2",
+                                                          "1_3", "1_4", "2_3", "2_4", 
+                                                          "3", "3_4", "4"))
+names(Tit_long)[3] <- "Total"
+
+ggplot(Tit_long, aes(x = Sentence, y = Total, fill = Regime)) +
+  geom_col(position = "fill") + 
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_manual(values = c("1" = "blue", "2" = "lightblue", "1_2" = "dodgerblue",
+                               "1_3" = "violet", "1_4" = "maroon2", "2_3" = "pink",
+                               "2_4" = "purple", "3" = "orange", "3_4" = "forestgreen", "4" = "red")) +
+  labs(title = "Titus (13 Book Pauline Corpus)")
+
 
 # Ephesians
 Eph_long <- Eph.long %>%
   count(Sentence, Regime) %>%
   complete(Sentence, Regime, fill = list(n = 0))
 
-Eph_long$Regime <- factor(Eph_long$Regime, levels = c("1_3", "2_3", "3", "1_2", "2", "1"))
+Eph_long$Regime <- factor(Eph_long$Regime, levels = c("1", "1_2", "2",
+                                                      "1_3", "1_4", "2_3", "2_4", 
+                                                      "3", "3_4", "4"))
 names(Eph_long)[3] <- "Total"
 
 ggplot(Eph_long, aes(x = Sentence, y = Total, fill = Regime)) +
   geom_col(position = "fill") + 
   scale_y_continuous(labels = scales::percent) +
-  scale_fill_manual(values = c("1" = "lightblue", "2" = "blue", 
-                               "3" = "orange", "1_2" = "forestgreen",
-                               "1_3" = "yellow", "2_3" = "violet")) +
+  scale_fill_manual(values = c("1" = "blue", "2" = "lightblue", "1_2" = "dodgerblue",
+                               "1_3" = "violet", "1_4" = "maroon2", "2_3" = "pink",
+                               "2_4" = "purple", "3" = "orange", "3_4" = "forestgreen", "4" = "red")) +
   labs(title = "Ephesians (13 Book Pauline Corpus)")
+
+# Colossians
+Col_long <- Col.long %>%
+  count(Sentence, Regime) %>%
+  complete(Sentence, Regime, fill = list(n = 0))
+
+Col_long$Regime <- factor(Col_long$Regime, levels = c("1", "1_2", "2",
+                                                      "1_3", "1_4", "2_3", "2_4", 
+                                                      "3", "3_4", "4"))
+names(Col_long)[3] <- "Total"
+
+ggplot(Col_long, aes(x = Sentence, y = Total, fill = Regime)) +
+  geom_col(position = "fill") + 
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_manual(values = c("1" = "blue", "2" = "lightblue", "1_2" = "dodgerblue",
+                               "1_3" = "violet", "1_4" = "maroon2", "2_3" = "pink",
+                               "2_4" = "purple", "3" = "orange", "3_4" = "forestgreen", "4" = "red")) +
+  labs(title = "Colossians (13 Book Pauline Corpus)")
+
+# 2 Thessalonians
+Thes.2_long <- Thes.2.long %>%
+  count(Sentence, Regime) %>%
+  complete(Sentence, Regime, fill = list(n = 0))
+
+Thes.2_long$Regime <- factor(Thes.2_long$Regime, levels = c("1", "1_2", "2",
+                                                      "1_3", "1_4", "2_3", "2_4", 
+                                                      "3", "3_4", "4"))
+names(Thes.2_long)[3] <- "Total"
+
+ggplot(Thes.2_long, aes(x = Sentence, y = Total, fill = Regime)) +
+  geom_col(position = "fill") + 
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_manual(values = c("1" = "blue", "2" = "lightblue", "1_2" = "dodgerblue",
+                               "1_3" = "violet", "1_4" = "maroon2", "2_3" = "pink",
+                               "2_4" = "purple", "3" = "orange", "3_4" = "forestgreen", "4" = "red")) +
+  labs(title = "2 Thessalonians (13 Book Pauline Corpus)")
