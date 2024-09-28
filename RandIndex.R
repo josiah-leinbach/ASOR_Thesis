7add0efb16ea0141588319e1af1e680e5251832f
josiah-leinbach <98821696+josiah-leinbach@users.noreply.github.com>
2024-09-28 15:10:58 -0400

    Update RandIndex.R

--- RandIndex.R
+++ RandIndex.R
@@ -1,8 +1,7 @@
 setwd("C:/Users/jleinba/Documents/ASOR_Thesis/Results/6-POS/R1.2-R2.2/EPS_10e-6")
 file.names <- list.files("C:/Users/jleinba/Documents/ASOR_Thesis/Results/6-POS/R1.2-R2.2/EPS_10e-6")
-file.order <- c(1741,1801,1066,1170,1215,1348,1415,1534,1556,1558,1605,
-                1611,1660,1662,1688,1697,1704,1707,1805,1815,
-                1945,2022,43,597,731)
+file.order <- c(1066,1170,1215,1348,1415,1534,1556,1588,1605,1611,1660,1662,1688,
+              1697,1704,1707,1741,1801,1805,1815,1945,2022,43,597,731)
 
 install.packages("MixSim")
 library(MixSim)
